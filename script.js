954e220c52aa5fb156cbd5008d4b2c4d858c6249
Dknowles <dknowles4489@gmail.com>
2022-01-23 20:01:28 -0500

    id and class overhaul

--- script.js
+++ script.js
@@ -1,41 +1,65 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
 const questions = [
     {
-        question: 'what is 1 + 1',
+        question: 'What does CSS stand for ?',
         answers: [
-            { text: '2', correct: true},
-            { text: '4', correct: false},
-            { text: '1', correct: false},
-            { text: '3', correct: false}
+            { text: 'cascading silly slides', correct: false},
+            { text: 'cascading style sheets', correct: true},
+            { text: 'chicken seasoned saturday', correct: false},
+            { text: 'catfish swim slow', correct: false}
 
         ]
     },
     
     {
-        question: 'what is 2 + 1',
+        question: 'What is JavaScript used for ?',
         answers: [
-            { text: '7', correct: true},
-            { text: '44', correct: false},
-            { text: '11', correct: false},
-            { text: '3', correct: false}
+            { text: 'allows you to make webpages interactive', correct: true},
+            { text: 'allows potatos to grow on wednesday', correct: false},
+            { text: 'style a webpage', correct: false},
+            { text: 'the right to party ?', correct: false}
 
         ]
     },
 
     {
-        question: 'what is 2 + 1',
+        question: 'What does DOM stand for ?',
         answers: [
-            { text: '77', correct: true},
-            { text: '44', correct: false},
-            { text: '111', correct: false},
-            { text: '33', correct: false}
+            { text: 'Document Occupancy Model', correct: false},
+            { text: 'Dollar Off Mondays', correct: false},
+            { text: 'Diversified Optical Manuvering', correct: false},
+            { text: 'Document Object Model', correct: true}
+
+        ]
+    },
+    
+    {
+        question: 'What is used most to add styling to a web page ?',
+        answers: [
+            { text: 'HTML', correct: false},
+            { text: 'JavaScript', correct: false},
+            { text: 'CSS', correct: true},
+            { text: 'API', correct: false}
+
+        ]
+    },
+
+    {
+        question: 'What HTML attribute references an external JavaScript file?',
+        answers: [
+            { text: 'src', correct: true},
+            { text: 'href', correct: false},
+            { text: 'index', correct: false},
+            { text: 'class', correct: false}
 
         ]
     }
+
 ]
 
 var shuffledQuestions, currentQuestionIndex
@@ -60,22 +84,58 @@ function startQuiz() {
 }
 
 function setNextQuestion() {
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(q) {
-    questionElement.innerText = 'question: ' + q.question
-    const question1 = document.getElementById('q-1');
-    question1.innerText = q.answers[0].text
-    const question2 = document.getElementById('q-2');
-    question2.innerText = q.answers[1].text
-    const question3 = document.getElementById('q-3');
-    question3.innerText = q.answers[2].text
-    const question4 = document.getElementById('q-4');
-    question4.innerText = q.answers[3].text
+    questionElement.innerText = q.question
+    q.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+   }
+
+   function resetState() {
+       nextButton.classList.add('hide')
+       while (answerButtonsElement.firstChild) {
+           answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+       }
    }
 
 
 function selectAnswer() {
+    const selectedButton = e.target
+    const correct = selectButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+} else {
+    startButton.innertext = 'Restart'
+    startButton.classList.remove('hide')
+}
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 
 }
